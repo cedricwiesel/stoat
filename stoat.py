b67084ac720ce67c22dec1c2e1760f3ce4401af1
crazyweazel <wieselcedric@gmail.com>
2021-05-10 01:24:19 +0200

    Update stoat.py

--- stoat.py
+++ stoat.py
@@ -133,7 +133,7 @@ async def ping(ctx):
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    if message.content == ("https://www.twitch.tv/domaxii"):
+    if "https://www.twitch.tv/domaxii" in message.content:
         await message.delete()
 
 #Bot Status  
