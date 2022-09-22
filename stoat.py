fcb6d6d9905c6213ae9641679e46cdc97f0d5ad0
crazyweazel <63046719+crazyweazel@users.noreply.github.com>
2022-09-22 18:50:20 +0200

    Update stoat.py

--- stoat.py
+++ stoat.py
@@ -205,9 +205,9 @@ async def on_member_remove(member):
 
 #Auto-Role
 @bot.event
-async def on_member_join(member, guild):
-    welcomerole = discord.utils.get(guild.roles, name = "Member")
-    await member.add_roles(welcomerole, reason = "Joined the Server", atomic = True)
+async def on_member_join(member, guild, ctx):
+    welcomerole = discord.utils.get(ctx.guild.roles, name = "Member")
+    await member.add_roles(welcomerole)
     await bot.process_commands()
 
 #mass-role-add
